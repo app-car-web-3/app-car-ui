79afa56e3a2520697ae79e2a891c5bbabfdcbb1d
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2024-06-08 00:04:36 +0200

    fix : Fix redirect page

--- src/app/component/page/home/Home.tsx
+++ src/app/component/page/home/Home.tsx
@@ -11,7 +11,7 @@ export default function Home() {
                     <p className="text-lg text-gray-300 mt-4">Discover our selection of top-quality cars.</p>
                     <br />
                     <br />
-                    <a href="/car-search" className="mt-8 px-6  py-3 bg-blue-600 text-white rounded hover:bg-blue-700">Car brand</a>
+                    <a href="/client/car-search" className="mt-8 px-6  py-3 bg-blue-600 text-white rounded hover:bg-blue-700">Car brand</a>
                 </div>
             </main>
         </div>
