af99ee32b41779bf9cc964fdadfe9e40364f544b
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2024-06-07 23:04:19 +0200

    feat: Create button logout

--- src/app/component/admin/Nav.tsx
+++ src/app/component/admin/Nav.tsx
@@ -51,7 +51,7 @@ const NavBar: React.FC = () => {
                             </li>
                             <li>
                                 <a href="/admin/cars" className="flex items-center p-2 text-white rounded-lg hover:bg-gray-700 group">
-                                      <svg className={`w-6 h-6 text-orange-800 dark:text-white block py-2 px-3 rounded md:p-0 ${pathname === '/admin/cars' ? 'text-orange bg-orange-700 md:bg-transparent md:text-orange-700 dark:text-white md:dark:text-orange-500' : 'text-white hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-700 dark:text-white md:dark:hover:text-orange-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'}`} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                    <svg className={`w-6 h-6 text-orange-800 dark:text-white block py-2 px-3 rounded md:p-0 ${pathname === '/admin/cars' ? 'text-orange bg-orange-700 md:bg-transparent md:text-orange-700 dark:text-white md:dark:text-orange-500' : 'text-white hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-700 dark:text-white md:dark:hover:text-orange-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'}`} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                         <path fill="currentColor" fill-rule="evenodd" d="M4 4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2H4Zm5.178 12.137a4.137 4.137 0 1 1 1.036-8.144A6.113 6.113 0 0 0 8.726 12c0 1.531.56 2.931 1.488 4.006a4.114 4.114 0 0 1-1.036.131ZM10.726 12c0-1.183.496-2.252 1.294-3.006A4.125 4.125 0 0 1 13.315 12a4.126 4.126 0 0 1-1.294 3.006A4.126 4.126 0 0 1 10.726 12Zm4.59 0a6.11 6.11 0 0 1-1.489 4.006 4.137 4.137 0 1 0 0-8.013A6.113 6.113 0 0 1 15.315 12Z" clip-rule="evenodd" />
                                     </svg>
                                     <span className={`block py-2 px-3 rounded md:p-0 ${pathname === '/admin/cars' ? 'text-orange bg-orange-700 md:bg-transparent md:text-orange-700 dark:text-white md:dark:text-orange-500' : 'text-white hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-700 dark:text-white md:dark:hover:text-orange-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'}`}>Car</span>
@@ -65,6 +65,12 @@ const NavBar: React.FC = () => {
                                     <span className={`block py-2 px-3 rounded md:p-0 ${pathname === '/admin/appointments' ? 'text-orange bg-orange-700 md:bg-transparent md:text-orange-700 dark:text-white md:dark:text-orange-500' : 'text-white hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-700 dark:text-white md:dark:hover:text-orange-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'}`}>Appointment</span>
                                 </a>
                             </li>
+                            <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded flex items-center">
+                                <svg className="w-6 h-6 mr-2 text-white-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 12H4m12 0-4 4m4-4-4-4m3-4h2a3 3 0 0 1 3 3v10a3 3 0 0 1-3 3h-2" />
+                                </svg>
+                                Logout
+                            </button>
                         </ul>
                     </div>
                 </aside>
