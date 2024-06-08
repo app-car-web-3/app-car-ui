dc17b01cbaf296bd2ed9008f01cdd3f1155e998c
junkaM <hei.mikanto.3@gmail.com>
2024-06-08 11:25:36 +0300

    fix: admin page

--- src/app/admin/dataProvider.js
+++ src/app/admin/dataProvider.js
@@ -1,12 +1,14 @@
 // import { fetchUtils } from 'ra-core';
 // import { simpleRestProvider } from 'ra-data-simple-rest';
 
-// const apiUrl = 'http://localhost:8080/api';
+
 // const httpClient = (url, options = {}) => {
 //     if (!options.headers) {
 //         options.headers = new Headers({ Accept: 'application/json' });
 //     }
+//     else return null;
 //     return fetchUtils.fetchJson(url, options);
+
 // };
 
 
